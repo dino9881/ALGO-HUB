31f666358114f868876ed26158eb40731c1272b9
dino9881 <76129597+dino9881@users.noreply.github.com>
2024-06-05 11:39:28 +0900

    [Gold III] Title: 파티, Time: 652 ms, Memory: 55032 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,85 +1,94 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /**
  * Main
  */
 public class Main {
 
-    static int N, M, X, ans;
-    static int dist[][];
-    static PriorityQueue<Pair> pq;
-    static List<List<Pair>> graph = new ArrayList<>();
+    static int N, M, X;
+    static int[] dist, ans;
+    static List<Data>[] graph;
 
     /**
-     * Pair
+     * Data
      */
-    public class Pair implements Comparable<Pair>{
-        
+    public class Data implements Comparable<Data> {
         int node, weight;
 
-        Pair(int node, int weight){
-            this.node = node;
-            this.weight = weight;
+        Data(int n, int w) {
+            node = n;
+            weight = w;
         }
 
-        @Override
-        public int compareTo(Main.Pair o) {
-            return Integer.compare(weight, o.weight);
+        public int compareTo(Data d) {
+            return Integer.compare(weight, d.weight);
         }
+
     }
 
-    public void solve(int start){
-        pq = new PriorityQueue<>();
-        pq.add(new Pair(start, 0));
-        while (!pq.isEmpty()){
-            Pair p = pq.poll();
-            int node = p.node;
-            int w = p.weight;
-            if (dist[start][node] > w){
-                dist[start][node] = w;
-                for (int i = 0; i < graph.get(node).size(); i++)
-                    pq.add(new Pair(graph.get(node).get(i).node, w + graph.get(node).get(i).weight));
+    // 시작점 ~ 도착점의 거리를 반환하는 다익스트라
+    public void dijk(int start) {
+        dist = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            dist[i] = 10000000;
+        }
+        dist[start] = 0;
+        PriorityQueue<Data> pq = new PriorityQueue<>();
+        pq.add(new Data(start, 0));
+        while (!pq.isEmpty()) {
+            Data data = pq.poll();
+            int node = data.node;
+            int weight = data.weight;
+            if (dist[node] < weight)
+                continue;
+            for (Data next : graph[node]) {
+                int nextNode = next.node;
+                int nextWeight = next.weight;
+                if (weight + nextWeight < dist[nextNode]) {
+                    dist[nextNode] = weight + nextWeight;
+                    pq.add(new Data(nextNode, weight + nextWeight));
+                }
             }
         }
     }
 
-    public void solution() throws Exception{
+    public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        
-        dist = new int[N + 1][N + 1];
-        for (int i = 0; i <= N; i++){
-            graph.add(new ArrayList<>());
-            for (int j = 0; j <= N; j++){
-                dist[i][j] = Integer.MAX_VALUE;
+
+        ans = new int[N + 1];
+        graph = new List[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph[s].add(new Data(e, w));
+        }
+        dijk(X);
+        ans = dist.clone();
+        for (int i = 1; i <= N; i++) {
+            if (i != X) {
+                dijk(i);
+                ans[i] += dist[X];
             }
         }
-        for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            int s, e, w;
-            s = Integer.parseInt(st.nextToken());
-            e = Integer.parseInt(st.nextToken());   
-            w = Integer.parseInt(st.nextToken());
-            graph.get(s).add(new Pair(e, w));
+        int ret = 0;
+        for (int i = 1; i <= N; i++) {
+            ret = Math.max(ret, ans[i]);
         }
-        for (int i = 1; i <= N; i++){
-            solve(i);
-        }
-        for (int i = 1; i <= N; i++){
-            ans = Math.max(dist[X][i] + dist[i][X], ans);
-        }
-        System.out.println(ans);
+        System.out.println(ret);
     }
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         new Main().solution();
     }
 }
