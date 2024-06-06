f7ff65afd6149d0fa3a80d05df390704d04f0e43
dino9881 <76129597+dino9881@users.noreply.github.com>
2024-06-06 11:42:51 +0900

    [Gold V] Title: 내려가기, Time: 308 ms, Memory: 46188 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -5,64 +5,43 @@ import java.util.*;
  * Main
  */
 public class Main {
-    static int N, min, max;
-    static int[] board;
-    static int[][] small, large;
-    final static int[] dc = { 1, 0, -1 };
+
+    static int[][] maxDp, minDp;
+    static int min, max;
+    static int N;
 
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
+        maxDp = new int[2][3];
+        minDp = new int[2][3];
         N = Integer.parseInt(br.readLine());
-        board = new int[3];
-        small = new int[2][3];
-        large = new int[2][3];
+        int x, y, z;
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 3; i++) {
-            board[i] = Integer.parseInt(st.nextToken());
-        }
-
-        for (int i = 0; i < 3; i++) {
-            small[0][i] = 10000000;
-            small[1][i] = board[i];
-            large[1][i] = board[i];
-        }
-
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N; i++) {
+            maxDp[0] = maxDp[1].clone();
+            minDp[0] = minDp[1].clone();
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                board[j] = Integer.parseInt(st.nextToken());
-            }
-            small[0] = small[1];
-            large[0] = large[1];
-            small[1] = new int[3];
-            large[1] = new int[3];
-            for (int j = 0; j < 3; j++) {
-                small[1][j] = 10000000;
-            }
-            for (int j = 0; j < 3; j++) {
-                for (int d = 0; d < 3; d++) {
-                    int idx = j + dc[d];
-                    if (idx < 0 || idx >= 3)
-                        continue;
-                    small[1][j] = Math.min(small[1][j], small[0][idx] + board[j]);
-                    large[1][j] = Math.max(large[1][j], large[0][idx] + board[j]);
-                }
-            }
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
+            z = Integer.parseInt(st.nextToken());
+            maxDp[1][0] = Math.max(maxDp[0][0], maxDp[0][1]) + x;
+            maxDp[1][1] = Math.max(Math.max(maxDp[0][0], maxDp[0][1]), maxDp[0][2]) + y;
+            maxDp[1][2] = Math.max(maxDp[0][1], maxDp[0][2]) + z;
+            minDp[1][0] = Math.min(minDp[0][0], minDp[0][1]) + x;
+            minDp[1][1] = Math.min(Math.min(minDp[0][0], minDp[0][1]), minDp[0][2]) + y;
+            minDp[1][2] = Math.min(minDp[0][1], minDp[0][2]) + z;
         }
         min = Integer.MAX_VALUE;
-        max = Integer.MIN_VALUE;
         for (int i = 0; i < 3; i++) {
-            max = Math.max(max, large[1][i]);
-            min = Math.min(min, small[1][i]);
+            min = Math.min(min, minDp[1][i]);
+            max = Math.max(max, maxDp[1][i]);
         }
         System.out.println(max + " " + min);
     }
 
     public static void main(String[] args) throws Exception {
         new Main().solution();
-
     }
 }
